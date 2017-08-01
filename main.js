91ececb7691ff5c0e9ec4b1c2d2fd20c14eabfa6
Dre mf Randaci <drerandaci@gmail.com>
2017-07-31 19:03:49 -0500

    added cat to html

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var cat = "fluffy";
 console.log("This is mf main.js", cat);
 console.log("fixed problem");
+
+let catzContainer = document.getElementById('catz');
+console.log(catzContainer);
+catzContainer.innerHTML = '<h1>' + cat + '</h1>';
